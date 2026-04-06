2db82ddbf18bdc4b5d72aecc2e1a110ca6c57ead
Shryansh Parashar <jaguar000212@gmail.com>
2026-04-06 18:34:39 +0530

    Bump version to v5.0.3
    - Update `versionCode` to 6 and `versionName` to "5.0.3" in `build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.jaguar.attendancetracker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "5.0"
+        versionCode = 6
+        versionName = "5.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
